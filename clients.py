48464e0651812a43b5f3fc8b24ca8e8f9b05c261
Javier Luraschi <jluraschi@gmail.com>
2026-08-23 12:37:32 -0700

    use configurable url for server proxy

--- clients.py
+++ clients.py
@@ -1,14 +1,16 @@
 from openai import OpenAI
 import os
 
+hal9_url = os.environ["HAL9_URL"]  # e.g. "https://api.hal9.com"
+
 # OpenAI Client
 openai_client = OpenAI(
-    base_url="https://api.hal9.com/proxy/server=https://api.openai.com/v1/",
-    api_key = os.environ['HAL9_TOKEN']
+    base_url=f"{hal9_url}/proxy/server=https://api.openai.com/v1/",
+    api_key=os.environ["HAL9_TOKEN"]
 )
 
 # Groq Client
 groq_client = OpenAI(
-    base_url="http://api.hal9.com/proxy/server=https://api.groq.com/openai/v1",
-    api_key = os.environ['HAL9_TOKEN']
+    base_url=f"{hal9_url}/proxy/server=https://api.groq.com/openai/v1",
+    api_key=os.environ["HAL9_TOKEN"]
 )
